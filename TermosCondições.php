28bd473ceab3481efbd162524348c8de55e28b19
TiagoDores <tiagodoresmp@gmail.com>
2022-06-03 12:28:26 +0200

    .

--- TermosCondições.php
+++ TermosCondições.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Watch shop | eCommers</title>
+    <title>To You Fashion | Termos &amp; condições</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="manifest" href="site.webmanifest">
